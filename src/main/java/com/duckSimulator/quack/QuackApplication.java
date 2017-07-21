f50a4aa93e4dd719c1a33fd5f53735053c43f22f
chadellison <chad.ellison0123@gmail.com>
2017-07-21 15:23:22 -0500

    initial commit

---
+++ src/main/java/com/duckSimulator/quack/QuackApplication.java
@@ -0,0 +1,12 @@
+package com.duckSimulator.quack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuackApplication.class, args);
+	}
+}
---
+++ src/test/java/com/duckSimulator/quack/QuackApplicationTests.java
@@ -0,0 +1,16 @@
+package com.duckSimulator.quack;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class QuackApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
